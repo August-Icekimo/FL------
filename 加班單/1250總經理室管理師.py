18601776b34aac4c85ce7682534813e9be3661e2
YR3158 <icekimo@mail.cepp.gov.tw>
2026-05-27 13:35:00 +0800

    feat: 	人事室人員(主任除外)所以有加班單簽核流程均須先經過林賴卿主任(增加林賴卿主任至簽核流程)後再到賴世明主任。

--- 加班單/1250總經理室管理師.py
+++ 加班單/1250總經理室管理師.py
@@ -5,5 +5,12 @@
 if nlevel < 700 and master != 4:
     return True
 
+if onlyHRDept==1: 
+    # 針對人事室人員中，職級在「副單位主管」(750)(含)以下，經過總經理室管理師簽核
+    if nlevel<=750: 
+        return True
+    # 人事室人員未符合上述條件的假單，則跳過此關
+    return False 
+
 # 若上述條件皆不滿足，預設跳過此關卡
 return False
