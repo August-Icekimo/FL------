656007917d4191532eb5cf746a700c02fdd1f88b
YR3158 <icekimo@mail.cepp.gov.tw>
2026-05-28 14:24:40 +0800

    docs: clarify approval conditions in 880二廠廠長.py

--- 加班單/880二廠廠長.py
+++ 加班單/880二廠廠長.py
@@ -1,11 +1,18 @@
-# 跳過單位主管
+# 若申請人職級為單位主管(800)含以上，跳過此關卡
 if nlevel >= 800:
     return False
-if master==4:
+
+# 若申請人所屬群組為總經理室人員(4)，跳過此關卡
+if master == 4:
     return False
-if rankAbove13==1:
+
+# 若申請人為13職等以上人員(rankAbove13=1)，跳過此關卡
+if rankAbove13 == 1:
     return False
-# 簽核層級 單位主管 AND 數位身分識別證製作中心
+
+# 若簽核部門為數位身分識別證製作中心(7) 且 申請人職級為單位主管(800)之下，需經二廠廠長審核
 if plevel == 7 and nlevel < 800:
     return True
+
+# 預設行為：其他情況跳過此關卡
 return False
