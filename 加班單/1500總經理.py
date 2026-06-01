2817db12c8fb7158850360f0dac08edb7d9af22e
YR3158 <icekimo@mail.cepp.gov.tw>
2026-06-01 14:23:02 +0800

    fix: 拼寫錯誤

--- 加班單/1500總經理.py
+++ 加班單/1500總經理.py
@@ -2,7 +2,7 @@
 if nlevel==800 or nlevel==900:
     return True
 # 審核副單位主管 AND (第一工廠 OR 第二工廠)
-if nlevel==750 and plevell in (1, 2):
+if nlevel==750 and plevel in (1, 2):
     return True
 # 特別取回總經理室13等以上人員
 if master==4 and rankAbove13==1:
